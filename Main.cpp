ced6501c5c5ad712bdcd8b588aa7d5cf221496df
FakeGath <givanalvaradoath@gmail.com>
2024-08-05 18:49:13 -0600

    this program is only for comments purpouses

---
+++ Main.cpp
@@ -0,0 +1,13 @@
+//This brings the iostream library
+#include <iostream>
+
+/*
+    This is a block comment that englobes multiple lines of code
+*/
+
+int main (){
+    //This code prints the phrase "Hello World" to the console.
+    std :: cout <<"Hello World!"<< std :: endl;
+    return 0;
+    //Program Ends here
+}
